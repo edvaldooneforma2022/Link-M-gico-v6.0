5775601743e8c28e2ed9125e157348eed98e9c77
edvaldooneforma2022 <edvaldooneforma2022@gmail.com>
2025-08-19 10:38:03 -0300

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit  # Para se der erro, parar imediatamente
 
+# Atualiza o pip
 pip install --upgrade pip
+
+# Instala as dependências do requirements_render.txt
 pip install -r requirements_render.txt
 
-# Instala somente o Chromium para evitar erro de permissão no Render
-python -m playwright install --with-deps chromium
+# Instala somente o Chromium (evita erro de permissão no Render)
+python -m playwright install chromium

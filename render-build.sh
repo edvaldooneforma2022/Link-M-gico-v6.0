ec77f16fdb73f99921d74155fd894649754a578f
edvaldooneforma2022 <edvaldooneforma2022@gmail.com>
2025-08-19 10:04:33 -0300

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -4,5 +4,5 @@ set -o errexit  # Para se der erro, parar imediatamente
 pip install --upgrade pip
 pip install -r requirements_render.txt
 
-# Instala navegadores do Playwright
-playwright install --with-deps
+# Instala somente o Chromium para evitar erro de permissão no Render
+python -m playwright install --with-deps chromium
